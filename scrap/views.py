52f07624976d8453d64776140baa66808379ff3b
Shiloli-r <45619599+Shiloli-r@users.noreply.github.com>
2020-04-21 23:29:32 +0300

    ready to upload

--- scrap/views.py
+++ scrap/views.py
@@ -7,7 +7,7 @@ from . import models
 # https://losangeles.craigslist.org/search/bbb?query=python+tutor&sort=rel&lang=en&cc=gb
 # BASE_URL = 'https://losangeles.craigslist.org/search/bbb?query={}'
 BASE_URL = 'https://losangeles.craigslist.org/search/?query={}'
-BASE_IMAGE_URL = 'https://images.craiglist.org/{}_300x300.jpg'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 
 # Create your views here.
@@ -39,7 +39,6 @@ def new_search(request):
             print(post_image_url)
         else:
             post_image_url = 'https://craigslist.org/images/peace.jpg'
-
 
         postings.append((post_title, post_url, post_price, post_image_url))  # append a tuple to the postings list
 
